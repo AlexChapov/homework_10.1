f4d103dd4b15581b921c43039323359a5c85a57b
Alexander <AlexChapovg@gmail.com>
2025-02-20 21:01:04 +0400

    add readme

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,16 @@
-from typing import List, Dict, Union
+from typing import Dict, List, Union
+
+list_of_dictionaries = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 
-list_of_dictionaries = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-
-def filter_by_state(list_of_dictionaries: list[dict], state:str="EXECUTED") -> tuple[list, list]:
+def filter_by_state(
+    list_of_dictionaries: list[dict], state: str = "EXECUTED"
+) -> tuple[list, list]:
     """Функция распределения по спискам"""
     new_list_1 = []
     new_list_2 = []
@@ -23,10 +26,13 @@ def filter_by_state(list_of_dictionaries: list[dict], state:str="EXECUTED") -> t
 print(filter_by_state(list_of_dictionaries))
 
 
-def sort_by_date(list_of_dictionaries: List[Dict[str,Union[str, int]]],
-                 revers_str: bool = True) -> List[Dict[str, Union[str,int]]]:
+def sort_by_date(
+    list_of_dictionaries: List[Dict[str, Union[str, int]]], revers_str: bool = True
+) -> List[Dict[str, Union[str, int]]]:
     """Функция возвращающая список отсортированный по дате"""
-    sorted_list = sorted(list_of_dictionaries, key=lambda x: x["date"], reverse=revers_str)
+    sorted_list = sorted(
+        list_of_dictionaries, key=lambda x: x["date"], reverse=revers_str
+    )
 
     return f"Сортировка по убыванию: {sorted_list}"
 
