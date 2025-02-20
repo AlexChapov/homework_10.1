7145d7b5d71431ebf96a51f7e75febb8180d9013
Alexander <AlexChapovg@gmail.com>
2025-02-20 20:27:18 +0400

    code writing analysis

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Union
 
+
 list_of_dictionaries = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -24,8 +25,10 @@ print(filter_by_state(list_of_dictionaries))
 
 def sort_by_date(list_of_dictionaries: List[Dict[str,Union[str, int]]],
                  revers_str: bool = True) -> List[Dict[str, Union[str,int]]]:
+    """Функция возвращающая список отсортированный по дате"""
     sorted_list = sorted(list_of_dictionaries, key=lambda x: x["date"], reverse=revers_str)
 
     return f"Сортировка по убыванию: {sorted_list}"
+
 
 print(sort_by_date(list_of_dictionaries))
