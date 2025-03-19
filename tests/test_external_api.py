c6a4fb37423bc94c485a849933c8fe3b5a16a87a
Alexander <AlexChapovg@gmail.com>
2025-03-19 23:02:11 +0400

    add_test_utils

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,14 +1,16 @@
 from src.external_api import currency_conversion, API_KEY
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
 
 def test_currency_conversion(transaction_api):
+    """Тест на проверку входных данных RUB"""
     rub_transaction = currency_conversion(transaction_api)
     assert rub_transaction == "31957.58"
 
 
 @patch("requests.get")
 def test_transaction_rub(mock_get):
+    """Проверка отправки запроса и получение ответа"""
     mock_get.return_value.json.return_value = {"result": 95.0}
     mock_get.return_value.status_code = 200
     transaction = {"operationAmount": {"amount": "1", "currency": {"code": "EUR"}}}
