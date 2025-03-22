9f1ef76a802944e7ae5cd56a9192f62b2b45538a
Alexander <AlexChapovg@gmail.com>
2025-03-22 16:30:52 +0400

    add_utils_log

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("../logs/utils.log")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def reading_json_file(operations: str) -> list[dict]:
@@ -6,22 +14,22 @@ def reading_json_file(operations: str) -> list[dict]:
     try:
         with open(operations, "r", encoding="utf-8") as f:
             content = f.read()
-        # print(f"Файл {operations} открыт на чтение")
+        logger.info(f"Файл {operations} открыт на чтение")
         """Если файл, указанный в переменной operations, не сущеcтвует"""
     except FileNotFoundError:
-        print(f"Файл {operations} не существует")
+        logger.error(f"Файл {operations} не существует")
         return []
 
     """Если json-файл (operations) пустой возвращается пустой список"""
     try:
         data = json.loads(content)
-        # print(f"Данные из файла {operations} прочитаны")
+        logger.info(f"Данные из файла {operations} прочитаны")
     except json.JSONDecodeError:
-        print(f"Ошибка чтения json из файла {operations}")
+        logger.error(f"Ошибка чтения json из файла {operations}")
         return []
     """Если содержимое файла не является списком, то вернется пустой список"""
     if not isinstance(data, list):
-        print(f"Содержимое файла {operations} не является объектом типа list")
+        logger.error(f"Содержимое файла {operations} не является объектом типа list")
         return []
 
     return data
