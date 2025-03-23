8336c565ca8c1bcce19a6b8abd8a13e8d0105cd6
Alexander <AlexChapovg@gmail.com>
2025-03-23 21:22:20 +0400

    add tests

--- src/transaction_reader.py
+++ src/transaction_reader.py
@@ -1,21 +1,16 @@
 import csv
+
 import pandas as pd
 
 
-def transaction_csv_read(transaction_csv: str) -> list[dict]:
+def read_csv(file_path: str) -> list[dict]:
     """Функция принимает в качестве аргумента путь к csv-файлу и возвращает его содержимое в виде списка словарей."""
-    with open(transaction_csv, "r", encoding="utf-8") as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         reader = csv.DictReader(f)
         return list(reader)
 
 
-print(transaction_csv_read("../data/transactions.csv"))
-
-
-def transaction_xlsx_read(transaction_xlsx: str) -> list[dict]:
+def read_excel(file_path: str) -> list[dict]:
     """Функция принимает в качестве аргумента путь к excel-файлу и возвращает его содержимое в виде списка словарей."""
-    excel_data = pd.read_excel(transaction_xlsx).to_dict(orient="records")
+    excel_data = pd.read_excel(file_path).to_dict(orient="records")
     return excel_data
-
-
-print(transaction_xlsx_read("../data/transactions_excel.xlsx"))
